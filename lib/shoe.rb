96d9320264de87bf732cdbfda82e2d1634347038
Charlie Knight <williamchknight@gmail.com>
2020-11-18 20:44:50 -0800

    done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,16 @@
+require 'pry' 
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end  
   end
 
   def cobble
@@ -12,3 +19,5 @@ class Shoe
   end
 
 end
+
+
